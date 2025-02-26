0d59be543b54112db0da5f511d62a6c977510611
rafay45 <rafayy579@gmail.com>
2025-02-26 11:43:27 +0500

    Some Changes

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function add(event) {
-  let getValue = document.getElementById('input').value;
-  event.preventDefault();
-  if (getValue === "12345678910") {
-    window.location.href = "home.html"
-  }
-  else {
-    document.getElementById('alert').style.display = "block"
-    document.getElementById('input').value = ""
-  }
-}
-
 function rangeCounter() {
   let range = document.getElementById('input').value;
   if (range < 10) {
